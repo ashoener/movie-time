253f26fedd1544e6606070fdb9ed1f348703ec3d
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-03 11:41:02 -0500

    Set up express erver

---
+++ index.js
@@ -0,0 +1,38 @@
+import connectSequelize from "connect-session-sequelize";
+import express from "express";
+import exphbs from "express-handlebars";
+import session, { Store } from "express-session";
+import { nanoid } from "nanoid";
+import db from "./config/connection.js";
+import routeLoader from "./lib/routeLoader.js";
+
+const SequelizeStore = connectSequelize(Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: process.env.SESSION_SECRET || nanoid(),
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db,
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const hbs = exphbs.create({ extname: ".hbs" });
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+
+app.use(express.static("public"));
+
+await routeLoader("controllers", app);
+
+await db.sync({ force: false });
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
