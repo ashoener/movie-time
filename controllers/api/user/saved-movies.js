0dfecab0ac5b699d57fcc95049a5a6b54d10c99f
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-05 19:39:01 -0600

    Allow additions of saved movies

--- controllers/api/user/saved-movies.js
+++ controllers/api/user/saved-movies.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { User, Movie } from "../../../models/index.js";
+import { User, Movie, SavedMovie } from "../../../models/index.js";
 
 import { handleError } from "../../../lib/utils.js";
 import requireLoggedInApi from "../../../lib/middleware/requireLoggedInApi.js";
@@ -8,7 +8,9 @@ import { Sequelize } from "sequelize";
 
 const router = Router({ mergeParams: true });
 
-router.get("/", requireLoggedInApi, async (req, res) => {
+router.use(requireLoggedInApi);
+
+router.get("/", async (req, res) => {
   try {
     const user = (
       await User.findByPk(req.session.user.id, {
@@ -28,7 +30,26 @@ router.get("/", requireLoggedInApi, async (req, res) => {
     ).get({ plain: true });
     res.json({ success: true, movies: user.saved_movies });
   } catch (err) {
-    console.log(err);
+    handleError(err, res);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const movie = await Movie.findByPk(req.body.movie_id, {
+      attributes: ["id"],
+    });
+    if (!movie)
+      return res.status(400).json({
+        success: false,
+        errors: ["Movie not found"],
+      });
+    await SavedMovie.create({
+      user_id: req.session.user.id,
+      movie_id: movie.id,
+    });
+    res.json({ success: true });
+  } catch (err) {
     handleError(err, res);
   }
 });
