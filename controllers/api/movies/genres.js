520f4d11b95f0d88581829c6620e6d7e30079525
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-06 11:30:08 -0600

    Require logged in for timeline and genre API route

--- controllers/api/movies/genres.js
+++ controllers/api/movies/genres.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { handleError } from "../../../lib/utils.js";
 import { Genre } from "../../../models/index.js";
+import requireLoggedInApi from "../../../lib/middleware/requireLoggedInApi.js";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/", requireLoggedInApi, async (req, res) => {
   try {
     const genres = await Genre.findAll({
       order: [["id", "ASC"]],
