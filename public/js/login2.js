56260e3115e4cf0f1475405c4476ca29ab7d0c41
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-11-06 13:09:05 -0600

    post method for login

--- public/js/login2.js
+++ public/js/login2.js
@@ -3,4 +3,12 @@ const loginForm = async (event) => {
 
     const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: { 'Content-Type' : 'application/json '},
+        });
+    }
 }
