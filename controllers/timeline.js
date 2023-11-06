2e194963dad8a7ad228789ef6de77363ef39d1fb
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-06 13:22:14 -0600

    Add timeline page route

---
+++ controllers/timeline.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import requireLoggedIn from "../lib/middleware/requireLoggedIn";
+
+const router = Router();
+
+router.get("/", requireLoggedIn, async (req, res) => {
+  res.render("timeline");
+});
+
+export default router;
