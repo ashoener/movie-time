0cf6b5eaae81f8b1f7d0b4e71810352093d5d032
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-11-06 12:06:13 -0600

    login fontend JS

---
+++ public/js/login2.js
@@ -0,0 +1,6 @@
+const loginForm = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+}
